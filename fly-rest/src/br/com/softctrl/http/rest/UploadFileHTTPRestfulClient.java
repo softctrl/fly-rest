7f1b500da29aa9ee36870f3ec32f7be58a1e5c2a
timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2015-08-05 10:16:38 -0300

    New handler for file upload.
    ps.: on development yet.

---
+++ fly-rest/src/br/com/softctrl/http/rest/UploadFileHTTPRestfulClient.java
@@ -0,0 +1,82 @@
+package br.com.softctrl.http.rest;
+
+import java.io.InputStream;
+
+import br.com.softctrl.http.rest.listener.RequestFinishedListener;
+import br.com.softctrl.http.rest.listener.ResponseErrorListener;
+import br.com.softctrl.http.rest.listener.ResponseListener;
+import static br.com.softctrl.http.util.StreamUtils.inputStreamToString;
+import static br.com.softctrl.http.util.StreamUtils.streamToByteArray;
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Carlos Timoshenko Rodrigues Lopes
+http://www.0x09.com.br
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
+ * @author carlostimoshenkorodrigueslopes@gmail.com
+ */
+public final class UploadFileHTTPRestfulClient extends AbstractHTTPRestfulClient<InputStream, String> {
+
+	/**
+	 * @param responseListener
+	 * @param responseErrorListener
+	 * @param requestFinishedListener
+	 */
+	public UploadFileHTTPRestfulClient(ResponseListener<String> responseListener,
+			ResponseErrorListener responseErrorListener, RequestFinishedListener<String> requestFinishedListener) {
+		super(responseListener, responseErrorListener, requestFinishedListener);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * br.com.softctrl.http.rest.AbstractHTTPRestfulClient#createRequest(br.com.
+	 * softctrl.http.rest.HttpMethod, java.lang.String, java.lang.Object,
+	 * br.com.softctrl.http.rest.Parameter[])
+	 */
+	@Override
+	protected Request<InputStream, String> createRequest(HttpMethod httpMethod, String url, InputStream body,
+			Parameter... parameters) {
+
+		final Request<InputStream, String> request = new Request<InputStream, String>(httpMethod, url, body) {
+			@Override
+			public Response<String> parseResponse(int statusCode, InputStream result) {
+				String _result = inputStreamToString(result);
+				return new Response<String>(statusCode, _result);
+			}
+			@Override
+			public byte[] bodyToByteArray() {
+				return streamToByteArray(getBody());
+			}
+		};
+		if (parameters != null && parameters.length > 0) {
+			for (Parameter parameter : parameters) {
+				request.addParameter(parameter);
+			}
+		}
+		return request;
+	}
+
+}
