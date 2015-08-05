9d8ebf4d63449fff7029622eb2092cfdc9b14f34
timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2015-08-05 10:13:18 -0300

    :D

--- fly-rest/src/br/com/softctrl/http/rest/Request.java
+++ fly-rest/src/br/com/softctrl/http/rest/Request.java
@@ -1,5 +1,7 @@
 package br.com.softctrl.http.rest;
 
+import java.io.InputStream;
+
 /*
 The MIT License (MIT)
 
@@ -28,37 +30,62 @@ SOFTWARE.
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Carlos Timoshenko Rodrigues Lopes
+http://www.0x09.com.br
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 /**
  * @author carlostimoshenkorodrigueslopes@gmail.com
  */
-public abstract class Request<T> {
+public abstract class Request<R, S> {
 
 	private HttpMethod mHttpMethod = HttpMethod.GET; // Default method
 	private String mUrl;
 	private Set<Parameter> mParameters = new HashSet<Parameter>();
 	private Set<Property> mProperties = new HashSet<Property>();
-	private T mBody;
+	private R mBody;
 
-	public Request(HttpMethod httpMethod, String url, T body) {
+	public Request(HttpMethod httpMethod, String url, R body) {
 		this.mHttpMethod = httpMethod;
 		this.mUrl = url;
 		this.mBody = body;
 	}
 
-	public Request<T> addParameter(String name, String value) {
+	public Request<R, S> addParameter(String name, String value) {
 		return this.addParameter(new Parameter(name, value));
 	}
 
-	public Request<T> addParameter(Parameter parameter) {
+	public Request<R, S> addParameter(Parameter parameter) {
 		this.mParameters.add(parameter);
 		return this;
 	}
 
-	public Request<T> addProperty(String name, String value) {
+	public Request<R, S> addProperty(String name, String value) {
 		return this.addProperty(new Property(name, value));
 	}
 
-	public Request<T> addProperty(Property property) {
+	public Request<R, S> addProperty(Property property) {
 		this.mProperties.add(property);
 		return this;
 	}
@@ -87,8 +114,16 @@ public abstract class Request<T> {
 	 * 
 	 * @return
 	 */
-	public T getBody() {
+	public R getBody() {
 		return mBody;
+	}
+	
+	/**
+	 * We need implement this method to get bytes from body.
+	 * @return
+	 */
+	public byte[] bodyToByteArray() {
+		throw new NoSuchMethodError("You need to implement this method.");
 	}
 
 	/**
@@ -120,9 +155,10 @@ public abstract class Request<T> {
 	/**
 	 * We need implement this method to parse the response.
 	 * 
-	 * @param response
+	 * @param statusCode
+	 * @param result
 	 * @return
 	 */
-	public abstract Response<T> parseResponse(int statusCode, String result);
+	public abstract Response<S> parseResponse(int statusCode, InputStream result);
 
 }
