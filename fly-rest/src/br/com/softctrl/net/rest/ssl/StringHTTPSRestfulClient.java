f8344d7760b2daf826db9583825390c635bbe28a
timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2015-08-31 09:54:01 -0300

    =D

--- fly-rest/src/br/com/softctrl/net/rest/ssl/StringHTTPSRestfulClient.java
+++ fly-rest/src/br/com/softctrl/net/rest/ssl/StringHTTPSRestfulClient.java
@@ -59,4 +59,34 @@ public class StringHTTPSRestfulClient extends AbstractHTTPSRestfulClient<String,
 		super(responseListener, responseErrorListener, requestFinishedListener);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * br.com.softctrl.net.rest.AbstractHTTPRestfulClient#createRequest(br.com.
+	 * softctrl.net.rest.HttpMethod, java.lang.String, java.lang.Object,
+	 * br.com.softctrl.net.rest.Parameter[])
+	 */
+	@Override
+	protected Request<String, String> createRequest(HttpMethod httpMethod, String url, String body,
+			Parameter... parameters) {
+		final Request<String, String> request = new Request<String, String>(httpMethod, url, body) {
+			@Override
+			public Response<String> parseResponse(int statusCode, InputStream result) {
+				return new Response<String>(statusCode, streamToString(result));
+			}
+
+			@Override
+			public byte[] bodyToByteArray() {
+				return (getBody() + "").getBytes();
+			}
+		};
+		if (parameters != null && parameters.length > 0) {
+			for (Parameter parameter : parameters) {
+				request.addParameter(parameter);
+			}
+		}
+		return request;
+	}
+
 }
